2660a5a06fa43ecaa35340ccaf418b82b7eea558
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-06-09 23:13:57 -0400

    authorization passed. MVP met

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const Users = require("./users-model");
+const restricted = require("../auth/res-middleware.js");
 
-router.get("/", (req,res) => {
+router.get("/", restricted, (req,res) => {
     Users.find()
     .then(users => {
         res.json(users);
